8e302deee6d7b3f952bb09528e36498391183518
Alex <akhakman@gmail.com>
2024-09-30 11:45:09 +0200

    add lazy loading

--- index.js
+++ index.js
@@ -90,60 +90,6 @@ lightboxNoScroll.addEventListener('click', (e) => {
     }
 });
 
-async function loadPhotos() {
-    const thumbnails = document.querySelectorAll('.thumbnail');
-
-    thumbnails.forEach((thumbnail, index) => {
-        thumbnail.addEventListener('click', () => {
-            openLightbox(thumbnail.getAttribute('data-src'), index);
-        });
-    });
-
-
-    // console.log(document.getElementsByClassName('thumbnail'))
-    // const thumbnails = document.getElementsByClassName('thumbnail');
-    // Array.from(thumbnails).forEach((thumbnail, index) => {
-    //     const imageUrl = `./images/Naamloos_HDR${index}.jpg`;
-    //     thumbnail.setAttribute('data-src', imageUrl);
-    // });
-    // for (let i = 1; i < ; i++) {
-    //     const imageUrl = `./images/Naamloos_HDR${i}.jpg?width=300&height=200`;
-    //     const img = document.createElement('img');
-    //     img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'; // Placeholder
-    //     img.setAttribute('data-src', imageUrl);
-    //     img.alt = `Casa Encanto foto ${i}`;
-    //     img.className = 'w-full h-full object-cover rounded-lg cursor-pointer lazy';
-    //     img.onclick = () => openLightbox(img.src.replace('width=300&height=200', 'width=1200&height=800'), images.length);
-    //     photoContainer.appendChild(img);
-    //     images.push(img);
-    // }
-    lazyLoad();
-}
-
-function checkImageExists(url) {
-    return new Promise((resolve) => {
-        const img = new Image();
-        img.onload = () => resolve(true);
-        img.onerror = () => resolve(false);
-        img.src = url;
-    });
-}
-
-function lazyLoad() {
-    const lazyImages = document.querySelectorAll('img.lazy');
-    const imageObserver = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                const image = entry.target;
-                image.src = image.getAttribute('data-src');
-                observer.unobserve(image);
-            }
-        });
-    });
-
-    lazyImages.forEach(img => imageObserver.observe(img));
-}
-
 
 function animateOnScroll() {
     const elements = document.querySelectorAll('.animate-on-scroll');
@@ -158,21 +104,11 @@ function animateOnScroll() {
     elements.forEach(element => {
         observer.observe(element);
     });
-
-
-
-
 }
-
-
-
-
-
 
 
 // Call functions when the page loads
 window.onload = async function() {
-    await loadPhotos();
     animateOnScroll();
 };
 
